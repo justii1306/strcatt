f0053578da2aa5799218ec9487cbb86283541d0e
justii1306 <35031460+justii1306@users.noreply.github.com>
2019-10-27 18:29:38 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *strcatt(char *str1, char *str2){
+    char *result;
+    int i=0,j=0,s;
+    s=strlen(str1)+strlen(str2)+1;
+    result=(char *)malloc(s*sizeof(char));
+    for(i;str1[i]!='\0';i++)
+        result[i]=str1[i];
+    for(j;str2!='\0';j++)
+        result[i+j]=str2[j];
+    return result;
+}
+
 int main()
 {
     char string1[] = "napis1";
@@ -12,15 +24,4 @@ int main()
     result=strcatt(string1,string2);
     printf("%s", &result);
     return 0;
-}
-char *strcatt(char *str1, char *str2){
-    char *result;
-    int i=0,j=0,s;
-    s=strlen(str1)+strlen(str2)+1;
-    result=(char *)malloc(s*siezeof(char));
-    for(i;str1[i]!='\n',i++)
-        result[i]=str1[i];
-    for(j;str2!='\n';j++)
-        result[i+j]=str2[j];
-    return result;
 }
